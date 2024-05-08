5bc987537dc1e78aa8744d26aa5d019e0878ec03
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-08 21:26:54 +0900

    feat : 레포지토리 연결

---
+++ src/test/java/com/linkode/api_server/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkode.api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
