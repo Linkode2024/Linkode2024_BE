f1193fff8c98220efe1c4944846561fa3857bfdb
Mouon <137624597+Mouon@users.noreply.github.com>
2024-06-26 23:24:59 +0900

    fix : 커밋 잘못
    레포지 클래스가 커밋되어서 수정

--- src/main/java/com/linkode/api_server/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.linkode.api_server.repository;
-
-import com.linkode.api_server.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
