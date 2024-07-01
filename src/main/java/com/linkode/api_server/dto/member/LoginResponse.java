f2cb032592e300f10229c3ae17b1f46c29cba8ed
jungeun <kje2066@naver.com>
2024-07-02 01:36:55 +0900

    feat : isMember -> memberStatus 로 변경
    isMember 로 하니까 인식을 잘못하는 오류발생

--- src/main/java/com/linkode/api_server/dto/member/LoginResponse.java
+++ src/main/java/com/linkode/api_server/dto/member/LoginResponse.java
@@ -1,9 +1,15 @@
 package com.linkode.api_server.dto.member;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginResponse {
     /**
      * 깃허브 소셜로그인
      */
-    private boolean isMember;
+    private boolean memberStatus;
     private String githubId;
 }
