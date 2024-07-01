0bace31afd6b31d473ad0d9b31237ed8d3695b84
jungeun <kje2066@naver.com>
2024-07-02 01:34:02 +0900

    feat : MemberRepository.existsByGithubIdAndStatus 작성

--- src/main/java/com/linkode/api_server/repository/MemberRepository.java
+++ src/main/java/com/linkode/api_server/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.linkode.api_server.repository;
 
 import com.linkode.api_server.domain.Member;
+import com.linkode.api_server.domain.base.BaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByGithubIdAndStatus(String githubId, BaseStatus ACTIVE);
 }
