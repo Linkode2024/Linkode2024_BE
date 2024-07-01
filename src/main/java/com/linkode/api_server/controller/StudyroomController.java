2f2b2fb3409e0bc1ef00b97bec4b0bd0fd4dcdab
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-02 01:04:20 +0900

    feat : 스터디룸 생성 논리흐름만 작성 (컨트롤러 계층)

--- src/main/java/com/linkode/api_server/controller/StudyroomController.java
+++ src/main/java/com/linkode/api_server/controller/StudyroomController.java
@@ -6,20 +6,18 @@ import com.linkode.api_server.service.StudyroomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/studyroom")
 public class StudyroomController {
 
     @Autowired
     StudyroomService studyroomService;
 
-    @PostMapping("/create")
+    @PostMapping("/generation")
     public CreateStudyroomResponse createStudyroom(@RequestBody CreateStudyroomRequest request){
         return studyroomService.createStudyroom(request);
     }
