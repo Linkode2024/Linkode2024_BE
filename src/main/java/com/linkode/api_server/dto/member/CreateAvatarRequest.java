78889ed29e758ac18bf5fb3dd8fbd89cc4f4e489
jungeun <kje2066@naver.com>
2024-07-04 00:46:29 +0900

    feat : 캐릭터 생성(회원가입) request dto 작성

---
+++ src/main/java/com/linkode/api_server/dto/member/CreateAvatarRequest.java
@@ -0,0 +1,16 @@
+package com.linkode.api_server.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAvatarRequest {
+
+    /**
+     * 캐릭터 생성 (회원 가입)
+     */
+    private String nickname;
+    private Long avatarId;
+    private String color;
+}
