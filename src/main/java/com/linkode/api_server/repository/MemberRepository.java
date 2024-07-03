ee1769f0512df37a36b2e1f603eb90701da78b0e
jungeun <kje2066@naver.com>
2024-07-04 01:23:35 +0900

    feat : memberId 로 토큰 생성
    기존에 githubId 로 토큰을 만들었는데 memberId 로 만들도록 수정

--- src/main/java/com/linkode/api_server/repository/MemberRepository.java
+++ src/main/java/com/linkode/api_server/repository/MemberRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByGithubIdAndStatus(String githubId, BaseStatus ACTIVE);
+    long findByGithubIdAndStatus(String githubId, BaseStatus ACTIVE);
 }
