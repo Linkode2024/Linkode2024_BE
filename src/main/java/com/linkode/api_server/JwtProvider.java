2fe89fdd40684695b94ef5f85d4e43bc33dc5cf8
jungeun <kje2066@naver.com>
2024-07-04 01:23:35 +0900

    feat : memberId 로 토큰 생성
    기존에 githubId 로 토큰을 만들었는데 memberId 로 만들도록 수정

--- src/main/java/com/linkode/api_server/JwtProvider.java
+++ src/main/java/com/linkode/api_server/JwtProvider.java
@@ -1,5 +1,7 @@
 package com.linkode.api_server;
 
+import com.linkode.api_server.domain.base.BaseStatus;
+import com.linkode.api_server.repository.MemberRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
@@ -7,9 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 @Component
 public class JwtProvider {
+    private final MemberRepository memberRepository;
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -20,8 +24,13 @@ public class JwtProvider {
     @Value("${jwt.refresh-token-expiration}")
     private long refreshTokenExpiration;
 
+    public JwtProvider(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // Access Token 생성
     public String createAccessToken(String githubId) {
+        long memberId = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE);
         Claims claims = Jwts.claims().setSubject(githubId);
         Date now = new Date();
         Date validity = new Date(now.getTime() + accessTokenExpiration);
@@ -30,12 +39,14 @@ public class JwtProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
+                .claim("memberId", memberId)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     // Refresh Token 생성
     public String createRefreshToken(String githubId) {
+        long memberId = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE);
         Claims claims = Jwts.claims().setSubject(githubId);
         Date now = new Date();
         Date validity = new Date(now.getTime() + refreshTokenExpiration);
@@ -44,6 +55,7 @@ public class JwtProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
+                .claim("memberId", memberId)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
@@ -59,7 +71,49 @@ public class JwtProvider {
     }
 
     // 토큰에서 사용자 정보 추출
-    public String getGithubId(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    public Long extractIdFromHeader(String authorization) {
+        // Authorization 헤더에서 JWT 토큰 추출
+        String jwtToken;
+        try {
+            jwtToken = extractJwtToken(authorization);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("유효하지 않은 헤더 포멧입니다.");
+        }
+
+        // JWT 토큰에서 사용자 정보 추출
+        Long memberId;
+        try {
+            memberId = extractMemberIdFromJwtToken(jwtToken);
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("토큰에서 멤버 아이디를 찾을 수 없습니다.");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("잘못된 토큰 형식입니다.");
+        }
+
+        return memberId;
+    }
+
+    public String extractJwtToken(String authorizationHeader) {
+        String[] parts = authorizationHeader.split(" ");
+        if (parts.length == 2) {
+            return parts[1]; // 토큰 부분 추출
+        }
+        throw new IllegalArgumentException("유효하지 않은 헤더 포멧입니다.");
+    }
+
+    public Long extractMemberIdFromJwtToken(String jwtToken) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+
+        Long memberId = claims.get("memberId", Long.class);
+
+        if (memberId == null) {
+            throw new NoSuchElementException("토큰에서 멤버 아이디를 찾을 수 없습니다.");
+        }
+
+        return memberId;
     }
 }
