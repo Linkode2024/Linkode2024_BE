fa6b671e6c79b590e8d3cbbd0fe292f75ae5e164
jungeun <kje2066@naver.com>
2024-07-04 20:14:42 +0900

    fix : JwtProvider 코드 수정

---
+++ src/main/java/com/linkode/api_server/util/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.linkode.api_server.util;
+
+import com.linkode.api_server.domain.Member;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Order(0)
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = parseBearerToken(request);
+        Member member = parseUserSpecification(token);
+        AbstractAuthenticationToken authenticated = new UsernamePasswordAuthenticationToken(member, null, null);
+        authenticated.setDetails(new WebAuthenticationDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticated);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .filter(token -> token.substring(0, 7).equalsIgnoreCase("Bearer "))
+                .map(token -> token.substring(7))
+                .orElse(null);
+    }
+
+    private Member parseUserSpecification(String token) {
+        String[] split = Optional.ofNullable(token)
+                .filter(subject -> subject.length() >= 10)
+                .map(jwtProvider::validateTokenAndGetSubject)
+                .orElse("anonymous:ANONYMOUS")
+                .split(":");
+
+        return new Member(null, split[0], "", "", null, null);
+    }
+}
