e1b655f077606474ed262cd1ce7aaefe74a34538
jungeun <kje2066@naver.com>
2024-07-04 19:35:01 +0900

    fix : 깃허브 소셜로그인 security 설정을 위한 코드 수정
    헤더로 들어온 토큰이 유효해야지만 해당 api 이용할 수 있도록 security 수정

--- src/main/java/com/linkode/api_server/config/SecurityConfig.java
+++ src/main/java/com/linkode/api_server/config/SecurityConfig.java
@@ -3,16 +3,23 @@ package com.linkode.api_server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .oauth2Login().authorizationEndpoint().baseUri("/login");
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/login","/oauth2/redirect").permitAll()	// requestMatchers의 인자로 전달된 url은 모두에게 허용
+                                .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )	// 세션을 사용하지 않으므로 STATELESS 설정
+                .build();
     }
 }
