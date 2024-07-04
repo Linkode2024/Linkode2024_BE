21792120e10be2fa5c6987af3c119d289ceb12bc
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-04 20:42:43 +0900

    feat : JWT 확인하도록 수정

--- src/main/java/com/linkode/api_server/controller/StudyroomController.java
+++ src/main/java/com/linkode/api_server/controller/StudyroomController.java
@@ -1,9 +1,9 @@
 package com.linkode.api_server.controller;
 
-import com.linkode.api_server.JwtProvider;
 import com.linkode.api_server.dto.CreateStudyroomRequest;
 import com.linkode.api_server.dto.CreateStudyroomResponse;
 import com.linkode.api_server.service.StudyroomService;
+import com.linkode.api_server.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +18,13 @@ public class StudyroomController {
     @Autowired
     StudyroomService studyroomService;
 
+    @Autowired
     JwtProvider jwtProvider;
 
     @PostMapping("/generation")
-    public CreateStudyroomResponse createStudyroom(@RequestHeader("Authorization") String authorization,  @RequestBody CreateStudyroomRequest request, @RequestParam long memberId){
+    public CreateStudyroomResponse createStudyroom(@RequestHeader("Authorization") String authorization,  @RequestBody CreateStudyroomRequest request){
         log.info("Success createStudyroom API");
-//        long memberId = jwtProvider.extractIdFromHeader(authorization);
+        long memberId = jwtProvider.extractIdFromHeader(authorization);
         return studyroomService.createStudyroom(request, memberId);
     }
 }
