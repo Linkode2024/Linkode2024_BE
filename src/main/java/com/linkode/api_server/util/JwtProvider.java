254c68c4622051168ee1875239df58cf4591dfa9
jungeun <kje2066@naver.com>
2024-07-04 19:35:01 +0900

    fix : 깃허브 소셜로그인 security 설정을 위한 코드 수정
    헤더로 들어온 토큰이 유효해야지만 해당 api 이용할 수 있도록 security 수정

--- src/main/java/com/linkode/api_server/util/JwtProvider.java
+++ src/main/java/com/linkode/api_server/util/JwtProvider.java
@@ -1,11 +1,14 @@
-package com.linkode.api_server;
+package com.linkode.api_server.util;
 
+import com.linkode.api_server.domain.Member;
 import com.linkode.api_server.domain.base.BaseStatus;
 import com.linkode.api_server.repository.MemberRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -30,7 +33,8 @@ public class JwtProvider {
 
     // Access Token 생성
     public String createAccessToken(String githubId) {
-        long memberId = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE);
+        Member member = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 깃허브 아이디로 유저를 찾을 수 없습니다.: " + githubId));
         Claims claims = Jwts.claims().setSubject(githubId);
         Date now = new Date();
         Date validity = new Date(now.getTime() + accessTokenExpiration);
@@ -39,14 +43,15 @@ public class JwtProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .claim("memberId", memberId)
+                .claim("memberId", member.getMemberId())
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     // Refresh Token 생성
     public String createRefreshToken(String githubId) {
-        long memberId = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE);
+        Member member = memberRepository.findByGithubIdAndStatus(githubId, BaseStatus.ACTIVE)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 깃허브 아이디로 유저를 찾을 수 없습니다.: " + githubId));
         Claims claims = Jwts.claims().setSubject(githubId);
         Date now = new Date();
         Date validity = new Date(now.getTime() + refreshTokenExpiration);
@@ -55,7 +60,7 @@ public class JwtProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .claim("memberId", memberId)
+                .claim("memberId", member.getMemberId())
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
@@ -71,49 +76,28 @@ public class JwtProvider {
     }
 
     // 토큰에서 사용자 정보 추출
-    public Long extractIdFromHeader(String authorization) {
-        // Authorization 헤더에서 JWT 토큰 추출
-        String jwtToken;
-        try {
-            jwtToken = extractJwtToken(authorization);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("유효하지 않은 헤더 포멧입니다.");
-        }
-
-        // JWT 토큰에서 사용자 정보 추출
-        Long memberId;
-        try {
-            memberId = extractMemberIdFromJwtToken(jwtToken);
-        } catch (NoSuchElementException e) {
-            throw new NoSuchElementException("토큰에서 멤버 아이디를 찾을 수 없습니다.");
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("잘못된 토큰 형식입니다.");
-        }
-
-        return memberId;
-    }
-
-    public String extractJwtToken(String authorizationHeader) {
-        String[] parts = authorizationHeader.split(" ");
-        if (parts.length == 2) {
-            return parts[1]; // 토큰 부분 추출
-        }
-        throw new IllegalArgumentException("유효하지 않은 헤더 포멧입니다.");
-    }
-
-    public Long extractMemberIdFromJwtToken(String jwtToken) {
-        Claims claims = Jwts.parserBuilder()
+    public String extractGithubId(String token) {
+        return Jwts.parserBuilder()
                 .setSigningKey(secretKey)
                 .build()
-                .parseClaimsJws(jwtToken)
-                .getBody();
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 
-        Long memberId = claims.get("memberId", Long.class);
+    // Validate JWT token
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String githubId = extractGithubId(token);
+        return (githubId.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
-        if (memberId == null) {
-            throw new NoSuchElementException("토큰에서 멤버 아이디를 찾을 수 없습니다.");
-        }
-
-        return memberId;
+    private boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
     }
 }
