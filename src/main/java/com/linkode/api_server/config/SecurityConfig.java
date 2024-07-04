b2711e5ee328bfcbe40c6413dd6a35d8810c5f4f
jungeun <kje2066@naver.com>
2024-07-04 20:14:42 +0900

    fix : JwtProvider 코드 수정

--- src/main/java/com/linkode/api_server/config/SecurityConfig.java
+++ src/main/java/com/linkode/api_server/config/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.linkode.api_server.config;
 
+import com.linkode.api_server.util.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .csrf(CsrfConfigurer<HttpSecurity>::disable)
                 .authorizeHttpRequests(requests ->
-                        requests.requestMatchers("/login","/oauth2/redirect").permitAll()	// requestMatchers의 인자로 전달된 url은 모두에게 허용
-                                .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+                        requests
+                                .requestMatchers("/login", "/oauth2/redirect").permitAll()  // 이 URL은 모두에게 허용
+                                .anyRequest().authenticated()  // 그 외의 모든 요청은 인증 필요
                 )
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )	// 세션을 사용하지 않으므로 STATELESS 설정
-                .build();
+                )  // 세션을 사용하지 않으므로 STATELESS 설정
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 추가
+
+        return http.build();
     }
 }
