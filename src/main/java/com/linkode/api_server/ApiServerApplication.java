2887da739fca3c1b2621d1734ea29df6e7e9830d
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-04 12:39:28 +0900

    feat : @EnableJpaAuditing 통해 생성일 수정일 자동생기도록함, API 정상작동 확인

--- src/main/java/com/linkode/api_server/ApiServerApplication.java
+++ src/main/java/com/linkode/api_server/ApiServerApplication.java
@@ -2,8 +2,10 @@ package com.linkode.api_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiServerApplication {
 
 	public static void main(String[] args) {
