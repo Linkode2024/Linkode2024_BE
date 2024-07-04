c21850a32490a04dad46b5f1fd79beb9e1ccea64
jungeun <kje2066@naver.com>
2024-07-05 00:10:14 +0900

    feat : MemberController.createAvatar 작성

---
+++ src/main/java/com/linkode/api_server/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.linkode.api_server.controller;
+
+import com.linkode.api_server.common.response.BaseResponse;
+import com.linkode.api_server.dto.member.CreateAvatarRequest;
+import com.linkode.api_server.service.MemberService;
+import com.linkode.api_server.util.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class MemberController {
+
+    @Autowired
+    private final MemberService memberService;
+
+    @Autowired
+    private final JwtProvider jwtProvider;
+
+    /**
+     * 캐릭터 생성(회원가입)
+     */
+    @PostMapping("/avatar")
+    public BaseResponse<Void> createAvatar(@RequestHeader("Authorization") String authorization,
+                                           @RequestBody CreateAvatarRequest createAvatarRequest) {
+        log.info("[MemberController.createAvatar]");
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        memberService.createAvatar(createAvatarRequest,memberId);
+        return new BaseResponse<>(null);
+    }
+}
