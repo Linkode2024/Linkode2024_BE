67697ea8435bc04e2eeb6f8ad4f760b76881efb2
jungeun <kje2066@naver.com>
2024-07-05 23:16:12 +0900

    feat : domain studyroom 에 Setter 추가

--- src/main/java/com/linkode/api_server/domain/Studyroom.java
+++ src/main/java/com/linkode/api_server/domain/Studyroom.java
@@ -8,12 +8,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Studyroom extends BaseTime {
