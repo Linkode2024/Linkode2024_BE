610aa07e7bf88e4f5c06b236a40082ba1ec3f7bd
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-07 16:21:09 +0900

    feat : 맴버아이디와 상태를 기반으로 찾는 레포지 메소드 추가

--- src/main/java/com/linkode/api_server/repository/AvatarRepository.java
+++ src/main/java/com/linkode/api_server/repository/AvatarRepository.java
@@ -2,8 +2,9 @@ package com.linkode.api_server.repository;
 
 import com.linkode.api_server.domain.Avatar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-
 
 }
--- src/main/java/com/linkode/api_server/repository/MemberRepository.java
+++ src/main/java/com/linkode/api_server/repository/MemberRepository.java
@@ -3,11 +3,15 @@ package com.linkode.api_server.repository;
 import com.linkode.api_server.domain.Member;
 import com.linkode.api_server.domain.base.BaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByGithubIdAndStatus(String githubId, BaseStatus ACTIVE);
     Optional<Member> findByGithubIdAndStatus(String githubId, BaseStatus ACTIVE);
+    Optional<Member> findByMemberIdAndStatus(long memberId, BaseStatus ACTIVE);
+
 }
