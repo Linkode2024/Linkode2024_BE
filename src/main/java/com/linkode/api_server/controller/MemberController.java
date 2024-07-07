f2b86c054b2ad2717921849b8a4526eed70fc775
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-07 16:22:29 +0900

    feat : 컨트롤러 계층 추가

--- src/main/java/com/linkode/api_server/controller/MemberController.java
+++ src/main/java/com/linkode/api_server/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.linkode.api_server.controller;
 import com.linkode.api_server.common.response.BaseResponse;
 import com.linkode.api_server.common.response.status.BaseExceptionResponseStatus;
 import com.linkode.api_server.dto.member.CreateAvatarRequest;
-import com.linkode.api_server.service.LoginService;
 import com.linkode.api_server.service.MemberService;
 import com.linkode.api_server.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +32,17 @@ public class MemberController {
     public BaseResponse<Void> createAvatar(@RequestBody CreateAvatarRequest createAvatarRequest) {
         log.info("[MemberController.createAvatar]");
         memberService.createAvatar(createAvatarRequest);
+        return new BaseResponse<>(null);
+    }
+
+    /**
+     * 캐릭터 수정(프로필 수정)
+     */
+    @PatchMapping("/avatar")
+    public BaseResponse<Void> modifyAvatar(@RequestHeader("Authorization") String authorization, @RequestBody UpdateAvatarRequest request) {
+        log.info("[MemberController.modifyAvatar]");
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        memberService.updateAvatar(memberId, request);
         return new BaseResponse<>(null);
     }
 
