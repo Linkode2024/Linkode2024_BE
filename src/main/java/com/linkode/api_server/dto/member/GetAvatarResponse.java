fb3d1855fad9f9c4df18763158a0614967ad19d0
jungeun <kje2066@naver.com>
2024-07-09 00:06:54 +0900

    feat : GetAvatarResponse dto 작성

---
+++ src/main/java/com/linkode/api_server/dto/member/GetAvatarResponse.java
@@ -0,0 +1,22 @@
+package com.linkode.api_server.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GetAvatarResponse {
+
+    /**
+     * 캐릭터 조회
+     */
+    private String nickname;
+    private Avatar avatar;
+
+    @Getter
+    @AllArgsConstructor
+    public static class Avatar{
+        private String avatarType;
+        private String avatarColor;
+    }
+}
