3a06dcd055d88c90657c9b6e303fdc7d73b5ba78
jungeun <kje2066@naver.com>
2024-07-09 14:16:40 +0900

    feat : MemberRepository.findByMemberIdWithAvatarAndStatus 작성

--- src/main/java/com/linkode/api_server/repository/MemberRepository.java
+++ src/main/java/com/linkode/api_server/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.linkode.api_server.repository;
 import com.linkode.api_server.domain.Member;
 import com.linkode.api_server.domain.base.BaseStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByGithubIdAndStatus(String githubId, BaseStatus status);
     Optional<Member> findByGithubIdAndStatus(String githubId, BaseStatus status);
     Optional<Member> findByMemberIdAndStatus(Long memberId, BaseStatus status);
+
+    @Query("SELECT m FROM Member m JOIN FETCH m.avatar WHERE m.memberId = :memberId AND m.status = :status")
+    Optional<Member> findByMemberIdWithAvatarAndStatus(Long memberId, BaseStatus status);
 }
