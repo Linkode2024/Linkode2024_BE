065e401ffd5bb48aef3d54546a9318629d2c6a55
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-18 20:12:37 +0900

    chore : 주석 추가

--- src/main/java/com/linkode/api_server/controller/StudyroomController.java
+++ src/main/java/com/linkode/api_server/controller/StudyroomController.java
@@ -119,6 +119,8 @@ public class StudyroomController {
 
     /***
      * 파일 업로드
+     * @RequestParam으로 쓴이유는 파일 업로드는 multipart/form-data 로 일반적 json이 아니기때문입니다.
+     * @RequestParam의 해당 값들이 URI에 노출되지않습니다.
      * */
     @PostMapping("/upload")
     public BaseResponse<UploadDataResponse> uploadData(@RequestHeader("Authorization") String authorization,
