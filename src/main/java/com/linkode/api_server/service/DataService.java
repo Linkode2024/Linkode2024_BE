3180b1c82f15428feb729844461325594efaf14b
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-21 15:49:04 +0900

    feat : 서비스 계층 작성

---
+++ src/main/java/com/linkode/api_server/service/DataService.java
@@ -0,0 +1,40 @@
+package com.linkode.api_server.service;
+
+import com.linkode.api_server.common.exception.DataException;
+import com.linkode.api_server.common.exception.MemberStudyroomException;
+import com.linkode.api_server.common.response.status.BaseExceptionResponseStatus;
+import com.linkode.api_server.domain.base.BaseStatus;
+import com.linkode.api_server.dto.studyroom.DataListResponse;
+import com.linkode.api_server.repository.DataRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.NOT_FOUND_DATA;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DataService {
+
+    private final DataRepository dataRepository;
+
+    public DataListResponse getDataList(long studyroomId, String type){
+        List<DataListResponse.Data> dataList= dataRepository.getDataListByType(studyroomId,type, BaseStatus.ACTIVE)
+                .orElseThrow(()->new DataException(NOT_FOUND_DATA))
+                .stream().map(data -> new DataListResponse.Data(
+                        data.getDataId(),
+                        data.getDataName(),
+                        data.getDataUrl()
+                )).collect(Collectors.toList());
+
+
+        return new DataListResponse(dataList);
+    }
+
+}
