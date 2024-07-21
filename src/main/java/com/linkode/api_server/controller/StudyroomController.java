a8b3d47e6036b61bdebd4ae39fb2f955565f43c0
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-07-21 15:49:19 +0900

    feat : 컨트롤러 계층 작성

--- src/main/java/com/linkode/api_server/controller/StudyroomController.java
+++ src/main/java/com/linkode/api_server/controller/StudyroomController.java
@@ -1,8 +1,10 @@
 package com.linkode.api_server.controller;
 
+import com.linkode.api_server.common.exception.DataException;
 import com.linkode.api_server.common.response.BaseResponse;
 import com.linkode.api_server.common.response.status.BaseExceptionResponseStatus;
 import com.linkode.api_server.dto.studyroom.*;
+import com.linkode.api_server.service.DataService;
 import com.linkode.api_server.service.MemberStudyroomService;
 import com.linkode.api_server.service.StudyroomService;
 import com.linkode.api_server.util.JwtProvider;
@@ -10,6 +12,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.NOT_FOUND_DATA;
+import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.SUCCESS;
 
 @RestController
 @Slf4j
@@ -22,18 +27,20 @@ public class StudyroomController {
     @Autowired
     MemberStudyroomService memberStudyroomService;
     @Autowired
+    DataService dataService;
+    @Autowired
     JwtProvider jwtProvider;
 
     /**
      * 스터디룸 삭제
      */
     @PatchMapping("/removal")
-    public BaseResponse<BaseExceptionResponseStatus> deleteStudyroom(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId){
+    public BaseResponse<BaseExceptionResponseStatus> deleteStudyroom(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId) {
 
         long memberId = jwtProvider.extractIdFromHeader(authorization);
-        BaseExceptionResponseStatus responseStatus = studyroomService.deleteStudyroom(studyroomId,memberId);
+        BaseExceptionResponseStatus responseStatus = studyroomService.deleteStudyroom(studyroomId, memberId);
         log.info("Run Delete Studyroom API ");
-        if (responseStatus == BaseExceptionResponseStatus.SUCCESS) {
+        if (responseStatus == SUCCESS) {
             return new BaseResponse<>(responseStatus);
         } else {
             return new BaseResponse<>(responseStatus, responseStatus);
@@ -42,19 +49,19 @@ public class StudyroomController {
 
     /**
      * 스터디룸 탈퇴
-     * */
+     */
     @PatchMapping("/leave")
-    public BaseResponse<MemberStudyroomListResponse> leaveStudyroom(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId){
+    public BaseResponse<MemberStudyroomListResponse> leaveStudyroom(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId) {
 
         long memberId = jwtProvider.extractIdFromHeader(authorization);
-        BaseExceptionResponseStatus responseStatus = memberStudyroomService.leaveStudyroom(studyroomId,memberId);
+        BaseExceptionResponseStatus responseStatus = memberStudyroomService.leaveStudyroom(studyroomId, memberId);
         MemberStudyroomListResponse latestStudyroomList = memberStudyroomService.getMemberStudyroomList(memberId);
         log.info("Run leaveStudyroom API ");
-        return new BaseResponse<>(responseStatus,latestStudyroomList);
+        return new BaseResponse<>(responseStatus, latestStudyroomList);
     }
 
     @PostMapping("/generation")
-    public CreateStudyroomResponse createStudyroom(@RequestHeader("Authorization") String authorization,  @RequestBody CreateStudyroomRequest request){
+    public CreateStudyroomResponse createStudyroom(@RequestHeader("Authorization") String authorization, @RequestBody CreateStudyroomRequest request) {
         log.info("Success createStudyroom API");
         long memberId = jwtProvider.extractIdFromHeader(authorization);
         return studyroomService.createStudyroom(request, memberId);
@@ -64,10 +71,10 @@ public class StudyroomController {
      * 스터디룸 수정
      */
     @PatchMapping("")
-    public BaseResponse<Void> modifyStudyroom(@RequestHeader("Authorization") String authorization, @RequestBody PatchStudyroomRequest patchStudyroomRequest){
+    public BaseResponse<Void> modifyStudyroom(@RequestHeader("Authorization") String authorization, @RequestBody PatchStudyroomRequest patchStudyroomRequest) {
         log.info("[StudyroomController.modifyStudyroom]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
-        studyroomService.modifyStudyroom(memberId,patchStudyroomRequest);
+        studyroomService.modifyStudyroom(memberId, patchStudyroomRequest);
         return new BaseResponse<>(null);
     }
 
@@ -75,10 +82,10 @@ public class StudyroomController {
      * 스터디룸 입장 (재입장)
      */
     @GetMapping("/detail")
-    public DetailStudyroomResponse getStudyroomDetail(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId){
+    public DetailStudyroomResponse getStudyroomDetail(@RequestHeader("Authorization") String authorization, @RequestParam long studyroomId) {
         log.info("[StudyroomController.getStudyroomDetail]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
-        return memberStudyroomService.getStudyroomDetail(studyroomId,memberId);
+        return memberStudyroomService.getStudyroomDetail(studyroomId, memberId);
     }
 
     /**
@@ -86,19 +93,34 @@ public class StudyroomController {
      */
     @PostMapping("/invite-code")
     public BaseResponse<PostInviteCodeResponse> createStudyroomCode(@RequestHeader("Authorization") String authorization,
-                                                                    @RequestParam Long studyroomId){
+                                                                    @RequestParam Long studyroomId) {
         log.info("[StudyroomController.createStudyroomCode]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
-        return new BaseResponse<>(studyroomService.createStudyroomCode(memberId,studyroomId));
+        return new BaseResponse<>(studyroomService.createStudyroomCode(memberId, studyroomId));
     }
 
     /**
      * 스터디룸 리스트 조회
      */
     @GetMapping("")
-    public BaseResponse<MemberStudyroomListResponse> getStudyroomList(@RequestHeader("Authorization") String authorization){
+    public BaseResponse<MemberStudyroomListResponse> getStudyroomList(@RequestHeader("Authorization") String authorization) {
         log.info("[StudyroomController.getStudyroomList]");
         Long memberId = jwtProvider.extractIdFromHeader(authorization);
         return new BaseResponse<>(memberStudyroomService.getStudyroomList(memberId));
+    }
+
+    /**
+     * 자료실 조회
+     */
+    @GetMapping("/data/list")
+    public BaseResponse<DataListResponse> getDataList(@RequestHeader("Authorization") String authorization,
+                                                      @RequestParam long studyroomId, @RequestParam String type) {
+        log.info("[StudyroomController.getDataList]");
+        try {
+            DataListResponse response = dataService.getDataList(studyroomId, type);
+            return new BaseResponse<>(SUCCESS, response);
+        } catch (DataException e) {
+            return new BaseResponse<>(NOT_FOUND_DATA, null);
+        }
     }
 }
