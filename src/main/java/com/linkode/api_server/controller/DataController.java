eff270567f06bde2b72e3b512f11040d3d6c9d22
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-08-13 14:47:02 +0900

    chore : 불필요한 임포트 및 애노테이션 제거

--- src/main/java/com/linkode/api_server/controller/DataController.java
+++ src/main/java/com/linkode/api_server/controller/DataController.java
@@ -4,41 +4,26 @@ import com.linkode.api_server.common.exception.DataException;
 import com.linkode.api_server.common.response.BaseResponse;
 import com.linkode.api_server.domain.data.DataType;
 import com.linkode.api_server.dto.studyroom.DataListResponse;
-import com.linkode.api_server.common.exception.MemberStudyroomException;
-import com.linkode.api_server.common.response.BaseResponse;
-import com.linkode.api_server.domain.data.DataType;
-import com.linkode.api_server.dto.studyroom.UploadDataRequest;
-import com.linkode.api_server.dto.studyroom.UploadDataResponse;
 import com.linkode.api_server.service.DataService;
-import com.linkode.api_server.service.MemberStudyroomService;
-import com.linkode.api_server.service.StudyroomService;
 import com.linkode.api_server.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.NOT_FOUND_DATA;
 import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.SUCCESS;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/studyroom")
+@RequestMapping("/studyroom/data")
 public class DataController {
-    @Autowired
     DataService dataService;
-    @Autowired
     JwtProvider jwtProvider;
 
     /**
      * 자료실 조회
      */
-    @GetMapping("/data/list")
+    @GetMapping("/list")
     public BaseResponse<DataListResponse> getDataList(@RequestHeader("Authorization") String authorization,
                                                       @RequestParam long studyroomId, @RequestParam DataType type) {
         log.info("[StudyroomController.getDataList]");
