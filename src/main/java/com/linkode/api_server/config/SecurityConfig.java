41052dffe96718a7be5a3846fdaa59fd4cbb1665
jungeun <kje2066@naver.com>
2024-08-13 17:07:10 +0900

    feat : cors 설정 추가

--- src/main/java/com/linkode/api_server/config/SecurityConfig.java
+++ src/main/java/com/linkode/api_server/config/SecurityConfig.java
@@ -23,19 +23,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(CsrfConfigurer<HttpSecurity>::disable)
-                .authorizeHttpRequests(requests ->
-                        requests
-                                .requestMatchers("/login", "/oauth2/redirect","/user/avatar","/linkode","/user/avatar/all","/ws/**").permitAll()  // 이 URL은 모두에게 허용
-                                .anyRequest().authenticated()  // 그 외의 모든 요청은 인증 필요
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // CORS 설정 적용
+                .csrf(CsrfConfigurer::disable)  // CSRF 비활성화
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/login", "/oauth2/redirect","/user/avatar","/linkode","/user/avatar/all","/ws/**").permitAll()  // 이 URL은 모두에게 허용
+                        .anyRequest().authenticated()  // 그 외의 모든 요청은 인증 필요
                 )
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )  // 세션을 사용하지 않으므로 STATELESS 설정
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션을 사용하지 않으므로 STATELESS 설정
+                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);  // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 추가
 
         return http.build();
     }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
