c8460c9f696c77923ad0f37a87c4ee74e0cf9748
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-08-27 18:51:32 +0900

    fix : 검증오류 수정

--- src/main/java/com/linkode/api_server/common/exception_handler/DataExceptionControllerAdvice.java
+++ src/main/java/com/linkode/api_server/common/exception_handler/DataExceptionControllerAdvice.java
@@ -1,23 +1,61 @@
 package com.linkode.api_server.common.exception_handler;
 
 import com.linkode.api_server.common.exception.DataException;
-import com.linkode.api_server.common.exception.MemberException;
 import com.linkode.api_server.common.response.BaseErrorResponse;
 import jakarta.annotation.Priority;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.FAILURE;
 
 @Slf4j
 @Priority(0)
 @RestControllerAdvice
 public class DataExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MemberException.class)
+    @ExceptionHandler({DataException.class})
     public BaseErrorResponse handle_DataException(DataException e) {
         log.error("[handle_DataException]", e);
         return new BaseErrorResponse(e.getExceptionStatus(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.error("[Validation Exception]", ex);
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String customMessage = String.format("'%s' 필드를 확인하세요.", field);
+            errors.put(field, customMessage);
+        });
+
+        String errorMessages = errors.entrySet().stream()
+                .map(entry -> String.format("%s: %s", entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining(", "));
+
+        return new BaseErrorResponse(FAILURE, errorMessages);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public BaseErrorResponse handleTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        log.error("[TypeMismatch Exception]", ex);
+        String errorMessage = String.format("잘못된 요청 파라미터입니다. '%s'는 '%s' 타입이어야 합니다.",
+                ex.getName(), ex.getRequiredType().getSimpleName());
+
+        return new BaseErrorResponse(FAILURE, errorMessage);
     }
 }
--- src/main/java/com/linkode/api_server/controller/DataController.java
+++ src/main/java/com/linkode/api_server/controller/DataController.java
@@ -1,21 +1,16 @@
 package com.linkode.api_server.controller;
 
-import com.linkode.api_server.common.exception.DataException;
-import com.linkode.api_server.common.exception.MemberStudyroomException;
 import com.linkode.api_server.common.response.BaseResponse;
 import com.linkode.api_server.domain.data.DataType;
 import com.linkode.api_server.dto.studyroom.DataListResponse;
 import com.linkode.api_server.dto.studyroom.UploadDataRequest;
 import com.linkode.api_server.dto.studyroom.UploadDataResponse;
-import com.linkode.api_server.handler.SignalingHandler;
 import com.linkode.api_server.service.DataService;
 import com.linkode.api_server.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.NOT_FOUND_DATA;
-import static com.linkode.api_server.common.response.status.BaseExceptionResponseStatus.SUCCESS;
 
 @RestController
 @Slf4j
@@ -33,16 +28,10 @@ public class DataController {
             @ModelAttribute UploadDataRequest request) {
 
         log.info("[StudyroomController.uploadData]");
-        try {
             Long memberId = jwtProvider.extractIdFromHeader(authorization);
             UploadDataResponse response = dataService.uploadData(request, memberId);
             dataService.broadCastUploadDataResponse(request.getStudyroomId(),memberId,response);
-            return new BaseResponse<>(SUCCESS, response);
-        } catch (DataException de) {
-            return new BaseResponse<>(de.getExceptionStatus(), null);
-        } catch (MemberStudyroomException me) {
-            return new BaseResponse<>(me.getExceptionStatus(), null);
-        }
+            return new BaseResponse<>(response);
     }
     /**
      * 자료실 조회
@@ -51,12 +40,8 @@ public class DataController {
     public BaseResponse<DataListResponse> getDataList(@RequestHeader("Authorization") String authorization,
                                                       @RequestParam long studyroomId, @RequestParam DataType type) {
         log.info("[StudyroomController.getDataList]");
-        try {
-            Long memberId = jwtProvider.extractIdFromHeader(authorization);
-            DataListResponse response = dataService.getDataList(memberId,studyroomId,type);
-            return new BaseResponse<>(SUCCESS, response);
-        } catch (DataException e) {
-            return new BaseResponse<>(NOT_FOUND_DATA, null);
-        }
+        Long memberId = jwtProvider.extractIdFromHeader(authorization);
+        DataListResponse response = dataService.getDataList(memberId,studyroomId,type);
+        return new BaseResponse<>(response);
     }
 }
