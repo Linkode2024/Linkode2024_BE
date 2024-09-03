ed89635de739e016692da8b1019ec588a3550b5a
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-09-03 20:51:44 +0900

    fix : 이슈 저장에 상태 추가

--- src/main/java/com/linkode/api_server/domain/GithubIssue.java
+++ src/main/java/com/linkode/api_server/domain/GithubIssue.java
@@ -1,5 +1,6 @@
 package com.linkode.api_server.domain;
 
+import com.linkode.api_server.domain.base.BaseStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,17 +20,20 @@ public class GithubIssue {
     private String body;
     private String url;
     private String state;
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    private BaseStatus status;
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "studyroom_id")
     private Studyroom studyroom;
 
     @Builder
-    public GithubIssue(String title, String body, String url, String state,Studyroom studyroom) {
+    public GithubIssue(String title, String body, String url, String state,BaseStatus status,Studyroom studyroom) {
         this.title = title;
         this.body = body;
         this.url = url;
         this.state = state;
+        this.status = status;
         this.studyroom=studyroom;
     }
 }
